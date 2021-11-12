96eb472298a30ebcf67ddadc2bbde4d4fdb87922
Hon <dennis.mejen@gmail.com>
2021-11-12 23:46:55 +0700

    [Added] 6 - Fetching Strapi Data

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const res = await fetch(url);
+        const json = await res.json();
+
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+};
+
+export default useFetch;
--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,3 +1,26 @@
+import { Link } from "react-router-dom";
+
+import useFetch from "../hooks/useFetch";
+
 export default function Homepage() {
-  return <div>Homepage</div>;
+  const { loading, error, data } = useFetch("http://localhost:1337/reviews");
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <div>
+      {data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.rating}</div>
+          <h2>{review.title}</h2>
+
+          <small>console list</small>
+
+          <p>{review.body.substring(0, 200)} ...</p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
