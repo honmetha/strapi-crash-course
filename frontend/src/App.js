ee3d09e10f4c17a5c1427916ab8ae63982127dc3
Hon <dennis.mejen@gmail.com>
2021-11-14 23:43:44 +0700

    [Added] 9 - Apollo Client Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // page & layout imports
 import Homepage from "./pages/Homepage";
@@ -6,17 +7,25 @@ import ReviewDetails from "./pages/ReviewDetails";
 import Category from "./pages/Category";
 import SiteHeader from "./components/SiteHeader";
 
+// apollo client
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <SiteHeader />
-        <Routes>
-          <Route exact path="/" element={<Homepage />} />
-          <Route path="/details/:id" element={<ReviewDetails />} />
-          <Route path="/category/:id" element={<Category />} />
-        </Routes>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <SiteHeader />
+          <Routes>
+            <Route exact path="/" element={<Homepage />} />
+            <Route path="/details/:id" element={<ReviewDetails />} />
+            <Route path="/category/:id" element={<Category />} />
+          </Routes>
+        </div>
+      </ApolloProvider>
     </Router>
   );
 }
