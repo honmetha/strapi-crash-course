555a979cc52651bfbe9bc16eed249ab81d4d0472
Hon <dennis.mejen@gmail.com>
2021-11-14 23:54:02 +0700

    [Added] 10 - The useQuery Hook

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
-import useFetch from "../hooks/useFetch";
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      title
+      body
+      rating
+      id
+    }
+  }
+`;
 
 export default function Homepage() {
-  const { loading, error, data } = useFetch("http://localhost:1337/reviews");
+  const { loading, error, data } = useQuery(REVIEWS);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <div>
-      {data.map((review) => (
+      {data.reviews.map((review) => (
         <div key={review.id} className="review-card">
           <div className="rating">{review.rating}</div>
           <h2>{review.title}</h2>
