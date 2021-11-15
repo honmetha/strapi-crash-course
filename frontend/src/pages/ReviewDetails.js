4ac7750a89d386e1bc1f8025559cca57ac3dbebf
Hon <dennis.mejen@gmail.com>
2021-11-15 23:47:36 +0700

    [Added] 11 - Query Variables

--- frontend/src/pages/ReviewDetails.js
+++ frontend/src/pages/ReviewDetails.js
@@ -1,23 +1,34 @@
 import { useParams } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
+import { useQuery, gql } from "@apollo/client";
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      title
+      body
+      rating
+      id
+    }
+  }
+`;
 
 export default function ReviewDetails() {
   const { id } = useParams();
-  const { loading, error, data } = useFetch(
-    "http://localhost:1337/reviews/" + id
-  );
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
 
   return (
     <div className="review-card">
-      <div className="rating">{data.rating}</div>
-      <h2>{data.title}</h2>
+      <div className="rating">{data.review.rating}</div>
+      <h2>{data.review.title}</h2>
 
       <small>console list</small>
 
-      <p>{data.body}</p>
+      <p>{data.review.body}</p>
     </div>
   );
 }
